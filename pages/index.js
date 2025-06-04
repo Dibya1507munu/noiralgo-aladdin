6b13ef52c81afefee0855e9a7b1511c9c38db22a
Dibya1507munu <djnwork1507@gmail.com>
2025-06-04 19:52:31 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
+      paddingTop: '100px'
+    }}>
+      <h1>🧠 NOIRALGO is Live!</h1>
+      <p>Start building your crypto signal engine here.</p>
+    </div>
+  );
+}
